46023181ef017b1ae1297dd275b0540f5cadcd27
Guggenbichler, Paul <p.guggenbichler@student.tugraz.at>
2026-02-18 17:10:15 +0100

    Finished cleanly importing data for further evaluation

--- Drag_function/config_utils_local/config.py
+++ Drag_function/config_utils_local/config.py
@@ -1,6 +1,9 @@
 import os
 
 BASE_PATH = r"C:\Users\paulg\Dokumente\Studium\Masterarbeit\Drag_Calculation\Data_DFT"
+BASE_PATH_OFFICE = r"C:\Users\u32f37\Documents\Masterarbeit\Drag_Calculation\Data_DFT"
 PATH9A  = os.path.join(BASE_PATH, "9A", "9A_All_Data.csv")
 PATH18A = os.path.join(BASE_PATH, "18A", "18A_All_Data.csv")
 PATH18A_OLD = os.path.join(BASE_PATH, "18A")
+PATH9A_OFFICE = os.path.join(BASE_PATH_OFFICE, "9A", "9A_All_Data.csv")
+PATH18A_OFFICE = os.path.join(BASE_PATH_OFFICE, "18A", "18A_All_Data.csv")
